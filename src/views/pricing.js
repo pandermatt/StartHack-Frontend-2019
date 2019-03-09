0ed8b021ec1ca072b415d1e14fe14212908ba202
Mischa Jörg <info@ninetyfour.ch>
2019-03-09 11:20:25 +0100

    change text

--- src/views/pricing.js
+++ src/views/pricing.js
@@ -15,25 +15,25 @@ class Pricing extends Component {
         </View>
         <PricingCard
           color={colors.primary}
-          title="Free"
-          price="$0"
-          info={['1 User', 'Basic Support', 'All Core Features']}
+          title="Starter"
+          price="$500"
+          info={['10 days / month', 'withouth fuel']}
           button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
           onButtonPress={() => addSubscription("subs1")}
         />
         <PricingCard
           color={colors.secondary}
-          title="Starter"
-          price="$19"
-          info={['10 Users', 'Basic Support', 'All Core Features']}
+          title="Basic"
+          price="$800"
+          info={['20 days / month', 'withouth fuel']}
           button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
           onButtonPress={() => addSubscription("subs2")}
         />
         <PricingCard
           color={colors.secondary2}
-          title="Enterprise"
-          price="$49"
-          info={['100 Users', 'One on One Support', 'All Core Features']}
+          title="Umlimited"
+          price="$1000"
+          info={['unlimited days / month', 'withouth fuel']}
           button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
           onButtonPress={() => addSubscription("subs3")}
         />
